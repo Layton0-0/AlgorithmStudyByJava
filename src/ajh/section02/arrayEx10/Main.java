944fb04ecc862074d2c100f5cbb6a741869edf15
Jinnny-An <58163606+Jinnny-An@users.noreply.github.com>
2023-01-09 22:20:52 +0900

    [지희] array10

--- src/ajh/section02/arrayEx10/Main.java
+++ src/ajh/section02/arrayEx10/Main.java
@@ -4,7 +4,14 @@ import java.util.*;
 public class Main {
     public int solution(int N, int[][] arr) {
         int answer = 0;
+        for (int i = 1; i < N+1 ; i++) {
+            for (int j = 1; j < N+1 ; j++) {
+                int target = arr[i][j];
+                // 위쪽 , 아랫쪽, 왼쪽, 오른쪽 비교
+                if ((arr[i-1][j] < target) && (arr[i+1][j] < target) && (arr[i][j-1] < target) && (arr[i][j+1] < target)) answer++;
 
+            }
+        }
         return answer;
     }
 
@@ -13,13 +20,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        int[][] arr = new int[n+2][n+2];
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++) {
-
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < n+1 ; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
+        System.out.print(T.solution(n, arr));
 
     }
 }
