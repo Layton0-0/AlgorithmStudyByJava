aa5d3c741bb95777f6593b2cc692e8be990acada
Jinnny-An <58163606+Jinnny-An@users.noreply.github.com>
2023-01-09 20:06:15 +0900

    [지희] 패키지 위치 변경

---
+++ src/ajh/section02/arrayEx10/Main.java
@@ -0,0 +1,25 @@
+package ajh.section02.arrayEx10;
+import java.util.*;
+
+public class Main {
+    public int solution(int N, int[][] arr) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++) {
+
+            }
+        }
+
+    }
+}
